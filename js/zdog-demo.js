91fc79e0e2a0775f1623d1b6bac87b499584aa3f
vickiliang <62454045+vickiliang@users.noreply.github.com>
2020-04-18 21:59:36 +1000

    trying hambergur

--- js/zdog-demo.js
+++ js/zdog-demo.js
@@ -1,25 +1,93 @@
-let illo = new Zdog.Illustration({
-  // set canvas with selector
+// Made with Zdog
+
+var yellow = '#ED0';
+var gold = '#EA0';
+var orange = '#E62';
+var garnet = '#C25';
+const TAU = Zdog.TAU;
+
+var illo = new Zdog.Illustration({
   element: '.zdog-canvas',
+  dragRotate: true,
+  rotate: { x: -TAU/8 },
 });
 
-// add circle
-new Zdog.Ellipse({
+var burger = new Zdog.Anchor({
   addTo: illo,
-  diameter: 80,
-  stroke: 20,
-  color: '#636',
+  translate: { y: 24 },
+  rotate: { x: TAU/4 },
 });
 
-// update & render
-illo.updateRenderGraph();
+// top bun
+var topBun = new Zdog.Hemisphere({
+  addTo: burger,
+  diameter: 96,
+  translate: { z: 44 },
+  stroke: 24,
+  color: orange,
+  // backface: gold,
+});
+
+// cheese
+new Zdog.Rect({
+  addTo: burger,
+  width: 92,
+  height: 92,
+  translate: { z: 24 },
+  stroke: 16,
+  color: yellow,
+  fill: true,
+});
+
+// patty
+new Zdog.Ellipse({
+  addTo: burger,
+  diameter: 96,
+  stroke: 32,
+  color: garnet,
+  fill: true,
+});
+
+// bottom bun
+new Zdog.Cylinder({
+  addTo: burger,
+  diameter: topBun.diameter,
+  length: 16,
+  translate: { z: -36 },
+  stroke: topBun.stroke,
+  color: topBun.color,
+});
+
+var seedAnchor = new Zdog.Anchor({
+  addTo: topBun,
+});
+
+var seedZ = ( topBun.diameter + topBun.stroke ) / 2 + 1;
+// seed
+new Zdog.Shape({
+  addTo: seedAnchor,
+  path: [ { y: -3 }, { y: 3 } ],
+  translate: { z: seedZ },
+  stroke: 8,
+  color: gold,
+});
+
+seedAnchor.copyGraph({
+  rotate: { x: 0.6 },
+});
+seedAnchor.copyGraph({
+  rotate: { x: -0.6 },
+});
+seedAnchor.copyGraph({
+  rotate: { y: -0.5 },
+});
+seedAnchor.copyGraph({
+  rotate: { y: 0.5 },
+});
 
 function animate() {
-  // rotate illo each frame
-  illo.rotate.y += 0.03;
   illo.updateRenderGraph();
-  // animate next frame
   requestAnimationFrame( animate );
 }
-// start animation
+
 animate();
